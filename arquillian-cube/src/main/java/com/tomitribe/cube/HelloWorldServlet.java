af4cb1929113d3daa30e2aa586712856b92e5d93
AndyGee <andy.is@gmx.de>
2015-04-21 11:42:27 +0200

    Just will not work on win

--- arquillian-cube/src/main/java/com/tomitribe/cube/HelloWorldServlet.java
+++ arquillian-cube/src/main/java/com/tomitribe/cube/HelloWorldServlet.java
@@ -10,5 +10,20 @@
  */
 package com.tomitribe.cube;
 
-public class HelloWorldServlet {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/HelloWorld")
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
+        final PrintWriter writer = resp.getWriter();
+        writer.println("Hello World");
+    }
 }
